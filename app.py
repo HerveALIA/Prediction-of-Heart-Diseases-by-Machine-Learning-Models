dca31b22f9c1f36b81bdef852b9a23363db12e6e
Hervé ALIA <94703843+HerveALIA@users.noreply.github.com>
2025-04-26 20:16:06 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Titre
+st.title('🔍 Prédiction de Maladie Cardiaque')
+
+# Message
+st.write('Bienvenue sur votre première application Streamlit !')
+
+# Exemple simple : bouton
+if st.button('Dire Bonjour'):
+    st.write('Bonjour médecin 👨‍⚕️👩‍⚕️!')
+
+
+
+
